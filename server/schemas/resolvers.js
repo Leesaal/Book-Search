599e3ee27bbeadbb89546427344a63ae5d511a8e
Leesa A <leesaali7@gmail.com>
2023-07-16 20:14:18 +0100

    added resolver for query me, mutations: login and adduser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,4 +2,36 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 const { signToken } = require("..utils/auth");
 
+const resolvers = {
+  Query: {
+    me: async (parents, args, context) => {
+      if (context.user) {
+        const userData = await User.findById(context.user._id);
+        return userData;
+      }
+      throw new AuthenticationError("You are not logged in!");
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No user with this email");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async(),
+  },
+};
+
 module.exports = resolvers;
